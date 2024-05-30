bbc7ae0359cb54c820a14c9768d3e5717777f770
Hassan Abouelela <hassan@hassanamr.com>
2024-05-30 20:17:40 +0400

    Add Redis

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import os
 
 import psycopg
+import redis
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify
 
 load_dotenv(".env")
 
+USE_CACHE = os.getenv("USE_CACHE", "true").lower() == "true"
+
 app = Flask(__name__)
 
-driver = psycopg.connect(os.getenv("DB_URI"))
+
+try:
+    driver = psycopg.connect(os.getenv("DB_URI"))
+except Exception as e:
+    raise Exception("Could not connect to postgres!") from e
+
+if USE_CACHE:
+    cache = redis.Redis.from_url(os.getenv("REDIS_URI"))
+    try:
+        cache.ping()
+    except Exception as e:
+        raise Exception("Could not connect to redis!", e)
 
 
 @app.route("/")
@@ -16,13 +30,27 @@ def hello_world():
     return "Hello, World!"
 
 
-@app.route("/db/<tem>")
-def read_database(tem: str):
-    result = driver.execute("SELECT (value) FROM public.important_data WHERE name=%s", (item,)).fetchone()
-    if result:
-        return f"Found result: {result[0]}"
-    else:
-        return "Item not found"
+@app.route("/db/<item>")
+def read_database(item: str):
+    result = src = None
+    if USE_CACHE:
+        result = cache.get(item)
+        if result:
+            result = result.decode("utf-8")
+        src = "redis"
+
+    if not result:
+        result = driver.execute("SELECT (value) FROM public.important_data WHERE name=%s;", (item,)).fetchone()
+        src = "database"
+
+        if not result:
+            return f"Item not found: {result}"
+
+        result = result[0]
+        if USE_CACHE:
+            cache.set(item, result)
+
+    return jsonify({"result": result, "src": src})
 
 
 if __name__ == '__main__':
